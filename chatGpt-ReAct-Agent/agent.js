e6c8291c131db2017cd2bb82ee470dee2a371d9e
Ali2220 <alisarwar0277@gmail.com>
2026-05-12 18:42:51 +0500

    add custom-calendar-tool in ReAct Agent

--- chatGpt-ReAct-Agent/agent.js
+++ chatGpt-ReAct-Agent/agent.js
@@ -3,6 +3,8 @@ dotenv.config()
 import { ChatGroq } from '@langchain/groq'
 import { createReactAgent } from '@langchain/langgraph/prebuilt';
 import { TavilySearch } from "@langchain/tavily";
+import { tool } from '@langchain/core/tools';
+import * as z from "zod";
 
 async function main() {
 
@@ -19,20 +21,50 @@ async function main() {
         topic: "general",
     });
 
+    // Custom Calendar Tool: 
+    const myEvents = [
+        { date: "2026-05-12", event: "AI Workshop at 10:00 AM" },
+        { date: "2026-05-13", event: "Meeting with Client at 2:00 PM" },
+        { date: "2026-05-15", event: "Exam Submission Deadline" }
+    ]
+
+    // tool function ke andar hum ek function dete hain, and aik object dete hain.
+    const calendarTool = tool(
+        async ({ date }) => {
+            console.log(`Checking calendar for: ${date}`);
+            const eventFound = myEvents.find(e => e.date === date)
+            return eventFound ? eventFound.event : "No events found for this date. You are free!";
+
+        },
+        {
+            name: "check_calendar",
+            description: "Use this tool to check for meetings, events, or deadlines on a specific date (format: YYYY-MM-DD).",
+            schema: z.object({
+                date: z.string().describe("The date to check in YYYY-MM-DD format."),
+            }),
+        }
+    );
+
     // STEP 3: Agent Creation
     // createReactAgent LLM aur Tools ko apas mein jorta hai.
     // Ye background mein ek "Reasoning" aur "Action" loop (ReAct) chalata hai.
     const agent = createReactAgent({
         llm: model,
-        tools: [search],
+        tools: [search, calendarTool],
     })
 
     // STEP 4: Invocation (Sawal Poochna)
     const result = await agent.invoke({
         messages: [
             {
+                role: "system",
+                content: `You are a helpful assistant. Use provided tools (search, calendarTool) if needed, else directly answer me if you have the information.
+                Date: ${new Date().toUTCString()}
+                `
+            },
+            {
                 role: "human",
-                content: "what is the current weather in Karachi"
+                content: "Do I have meeting tomorrow ?"
             }
         ]
     })
