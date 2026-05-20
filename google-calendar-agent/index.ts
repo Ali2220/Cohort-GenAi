721808d804e53c6c8f600a10e191ecdffa744788
Ali2220 <alisarwar0277@gmail.com>
2026-05-20 15:24:29 +0500

    Project: Google Calendar LLM setup

---
+++ google-calendar-agent/index.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import { ChatGroq } from '@langchain/groq'
+
+const tools: any = []
+
+const model = new ChatGroq({
+    apiKey: process.env.GROQ_API_KEY as string,
+    model: "llama-3.3-70b-versatile",
+    temperature: 0,
+}).bindTools(tools)
