35c220a2894859ce9efb8b42ed178821ae8573e3
Ali2220 <alisarwar0277@gmail.com>
2026-05-20 15:41:39 +0500

    Project: Google Calendar Tools setup and bind tools in LLM

--- google-calendar-agent/index.ts
+++ google-calendar-agent/index.ts
@@ -1,11 +1,14 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import { ChatGroq } from '@langchain/groq'
+import { createEvent, getEvents } from './tools.js'
 
-const tools: any = []
+const tools: any = [createEvent, getEvents]
 
 const model = new ChatGroq({
     apiKey: process.env.GROQ_API_KEY as string,
-    model: "llama-3.3-70b-versatile",
+    model: "openai/gpt-oss-120b",
     temperature: 0,
 }).bindTools(tools)
+
+
---
+++ google-calendar-agent/tools.ts
@@ -0,0 +1,33 @@
+import { tool } from '@langchain/core/tools'
+import z from 'zod';
+
+export const createEvent = tool(
+    async () => {
+        return "The event has been created"
+    },
+    {
+        name: 'create-event', // tool ka naam
+        description: 'Call to create the calendar event',
+        schema: z.object({
+        }),
+    }
+);
+
+export const getEvents = tool(
+    async () => {
+        return JSON.stringify([
+            {
+                title: 'Meeting with Ali',
+                date: '20th May 2026',
+                time: '5 PM',
+                location: 'Mama chai junction'
+            }
+        ])
+    },
+    {
+        name: 'get-events',
+        description: 'Call to get the calendar events',
+        schema: z.object({
+        }),
+    }
+);
