f5830c530f52599568849a0373b9f1329d028d7e
Ali2220 <alisarwar0277@gmail.com>
2026-06-22 18:27:48 +0500

    Project (research agent): create questionAndAnswer schema for responder agent and create responder agent

---
+++ research_agent/src/graph.ts
@@ -0,0 +1,37 @@
+import { AIMessage } from "@langchain/core/messages";
+import { model } from "./model.js";
+import { graphState, questionAnswerSchema } from "./state.js";
+import { StateGraph } from "@langchain/langgraph";
+
+async function responder(state: typeof graphState.State) {
+
+    const currentDateTime = new Date().toLocaleString("sv-SE")
+
+    const SYSTEM_PROMPT = `You are an expert researcher.
+Current time: ${currentDateTime}
+
+1. Provide a detailed ~250 word answer.
+2. Reflect and critique your answer. Be severe to maximize improvement.
+3. Recommend max 3 search queries to research information and improve your answer.`
+
+    const llmWithStructure = await model.withStructuredOutput(questionAnswerSchema)
+
+    const response = await llmWithStructure.invoke([
+        {
+            role: "system",
+            content: SYSTEM_PROMPT
+        },
+        ...state.messages,
+        {
+            role: 'system',
+            content: "Reflect on the user's original question and the actions taken thus far. Respond using structured output."
+        }
+    ])
+
+    return {
+        messages: new AIMessage(JSON.stringify(response))
+    }
+}
+
+const workflow = new StateGraph(graphState)
+.addNode('responder', responder)
--- research_agent/src/model.ts
+++ research_agent/src/model.ts
@@ -3,7 +3,7 @@ dotenv.config()
 import { ChatGroq } from "@langchain/groq"
 
 export const model = new ChatGroq({
-    model: "llama-3.3-70b-versatile",
+    model: "openai/gpt-oss-20b",
     apiKey: process.env.GROQ_API_KEY as string,
     temperature: 0
 })
