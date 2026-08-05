cbc0fdda4d36f18c297c1d18b4175f04add83edc
Ali2220 <alisarwar0277@gmail.com>
2026-08-05 19:08:43 +0500

    Project (expense tracker): setup ui components

--- expense tracker - gen ui/client/src/App.tsx
+++ expense tracker - gen ui/client/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
+import { ChatContainer } from "./components/ChatContainer";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-500">Hello</h1>
+      <ChatContainer />
     </>
   );
 }
