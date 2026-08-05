80bd309644ad89b4a4c301f4ce92f6b835f7d955
Ali2220 <alisarwar0277@gmail.com>
2026-08-05 18:22:07 +0500

    Project (expense tracker): setup tailwind css

--- expense tracker - gen ui/client/src/App.tsx
+++ expense tracker - gen ui/client/src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>Hello</h1>
+      <h1 className="text-3xl font-bold text-red-500">Hello</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
